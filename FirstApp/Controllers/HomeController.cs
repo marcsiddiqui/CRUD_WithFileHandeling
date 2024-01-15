9dd92c06943a8c0adb6c325a01beaf307adcf139
marcsiddiqui <marcsiddiqui@gmail.com>
2024-01-15 09:23:31 +0500

    crud with json

--- FirstApp/Controllers/HomeController.cs
+++ FirstApp/Controllers/HomeController.cs
@@ -4,6 +4,8 @@ using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
 using System.Linq;
+using Newtonsoft.Json;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace FirstApp.Controllers
 {
@@ -56,6 +58,8 @@ namespace FirstApp.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
+        #region Saving In File With custom working
+
         public IActionResult StudentList()
         {
             var model = new StudentListModel();
@@ -84,6 +88,7 @@ namespace FirstApp.Controllers
                             s_model.Class = lineData[3];
                             s_model.Section = lineData[4];
                             s_model.Email = lineData[5];
+                            s_model.PhoneNumber = lineData[6];
 
                             model.Students.Add(s_model);
                         }
@@ -131,7 +136,7 @@ namespace FirstApp.Controllers
 
                 var content = System.IO.File.ReadAllText(filePath);
 
-                var studentline = newLineNumber + "," + model.FullName + "," + model.FatherName + "," + model.Class + "," + model.Section + "," + model.Email;
+                var studentline = newLineNumber + "," + model.FullName + "," + model.FatherName + "," + model.Class + "," + model.Section + "," + model.Email + "," + model.PhoneNumber;
 
                 content = content + "\n" + studentline;
 
@@ -167,6 +172,7 @@ namespace FirstApp.Controllers
                         s_model.Class = lineData[3];
                         s_model.Section = lineData[4];
                         s_model.Email = lineData[5];
+                        s_model.PhoneNumber = lineData[6];
                         break;
                     }
 
@@ -200,7 +206,7 @@ namespace FirstApp.Controllers
                             var lineNumber = Convert.ToInt32(lineData[0]);
                             if (lineNumber == model.Id)
                             {
-                                var studentline = lineNumber + "," + model.FullName + "," + model.FatherName + "," + model.Class + "," + model.Section + "," + model.Email;
+                                var studentline = lineNumber + "," + model.FullName + "," + model.FatherName + "," + model.Class + "," + model.Section + "," + model.Email + "," + model.PhoneNumber;
                                 finalData = finalData + "\n" + studentline;
                             }
                             else
@@ -217,5 +223,208 @@ namespace FirstApp.Controllers
 
             return View(model);
         }
+
+        public IActionResult DeleteStudent(int id)
+        {
+            var filePath = "C:\\Users\\marsalan\\Desktop\\StudentData.txt";
+
+            if (!System.IO.File.Exists(filePath))
+                return RedirectToAction("StudentList");
+
+            var finalData = "";
+            if (System.IO.File.Exists(filePath))
+            {
+                var allLines = System.IO.File.ReadAllLines(filePath).ToList();
+                if (allLines != null && allLines.Any())
+                {
+                    foreach (var line in allLines)
+                    {
+                        if (!string.IsNullOrWhiteSpace(line))
+                        {
+                            var lineData = line.Split(",");
+
+                            var lineNumber = Convert.ToInt32(lineData[0]);
+                            if (lineNumber == id)
+                            {
+                                continue;
+                            }
+                            else
+                            {
+                                finalData = finalData + "\n" + line;
+                            }
+                        }
+                    }
+
+                    System.IO.File.WriteAllText(filePath, finalData);
+                    return RedirectToAction("StudentList");
+                }
+            }
+
+            return RedirectToAction("StudentList");
+        }
+
+        #endregion
+
+        #region Saving In File With Newtonsoft.Json package
+
+        public IActionResult StudentList2()
+        {
+            var model = new StudentListModel();
+
+            var filePath = "C:\\Users\\marsalan\\Desktop\\StudentData2.txt";
+
+            if (System.IO.File.Exists(filePath))
+            {
+                var content = System.IO.File.ReadAllText(filePath);
+
+                var list = !string.IsNullOrWhiteSpace(content) ? JsonConvert.DeserializeObject<List<StudentModel>>(content) : new List<StudentModel>();
+
+                if (list != null && list.Any())
+                    model.Students.AddRange(list);
+            }
+
+            return View(model);
+        }
+
+        public IActionResult CreateStudent2()
+        {
+
+
+
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent2(StudentModel model)
+        {
+            var filePath = "C:\\Users\\marsalan\\Desktop\\StudentData2.txt";
+
+            if (!System.IO.File.Exists(filePath))
+            {
+                System.IO.File.Create(filePath);
+            }
+
+            if (System.IO.File.Exists(filePath))
+            {
+                var content = System.IO.File.ReadAllText(filePath);
+
+                var list = !string.IsNullOrWhiteSpace(content) ? JsonConvert.DeserializeObject<List<StudentModel>>(content) : new List<StudentModel>();
+
+                if (list == null)
+                {
+                    list = new List<StudentModel>();
+                }
+
+                model.Id = list.OrderByDescending(x => x.Id).FirstOrDefault()?.Id + 1 ?? 1;
+
+                list.Add(model);
+
+                if (list != null && list.Any())
+                {
+                    var newContent = JsonConvert.SerializeObject(list);
+                    if (!string.IsNullOrWhiteSpace(newContent))
+                    {
+                        System.IO.File.WriteAllText(filePath, newContent);
+
+                        return RedirectToAction("StudentList2");
+                    }
+                }
+            }
+
+            return View();
+        }
+
+        public IActionResult EditStudent2(int id)
+        {
+            var filePath = "C:\\Users\\marsalan\\Desktop\\StudentData2.txt";
+            var content = System.IO.File.ReadAllText(filePath);
+
+            var list = !string.IsNullOrWhiteSpace(content) ? JsonConvert.DeserializeObject<List<StudentModel>>(content) : new List<StudentModel>();
+
+            if (list != null && list.Any())
+            {
+                var s_model = list.FirstOrDefault(x => x.Id == id);
+
+                if (s_model != null)
+                    return View(s_model);
+            }
+
+            return RedirectToAction("StudentList2");
+           
+        }
+
+        [HttpPost]
+        public IActionResult EditStudent2(StudentModel model)
+        {
+            var filePath = "C:\\Users\\marsalan\\Desktop\\StudentData2.txt";
+
+            if (!System.IO.File.Exists(filePath))
+                return RedirectToAction("StudentList");
+
+            if (System.IO.File.Exists(filePath))
+            {
+                var content = System.IO.File.ReadAllText(filePath);
+
+                var list = !string.IsNullOrWhiteSpace(content) ? JsonConvert.DeserializeObject<List<StudentModel>>(content) : new List<StudentModel>();
+
+                if (list != null && list.Any())
+                {
+                    list = list.Where(x => x.Id != model.Id).ToList();
+
+                    list.Add(model);
+
+                    list = list.OrderBy(x => x.Id).ToList();
+                }
+
+                if (list != null && list.Any())
+                {
+                    var newContent = JsonConvert.SerializeObject(list);
+                    if (!string.IsNullOrWhiteSpace(newContent))
+                    {
+                        System.IO.File.WriteAllText(filePath, newContent);
+
+                        return RedirectToAction("StudentList2");
+                    }
+                }
+            }
+
+            return View(model);
+        }
+
+        public IActionResult DeleteStudent2(int id)
+        {
+            var filePath = "C:\\Users\\marsalan\\Desktop\\StudentData2.txt";
+
+            if (!System.IO.File.Exists(filePath))
+                return RedirectToAction("StudentList2");
+
+            if (System.IO.File.Exists(filePath))
+            {
+                var content = System.IO.File.ReadAllText(filePath);
+
+                var list = !string.IsNullOrWhiteSpace(content) ? JsonConvert.DeserializeObject<List<StudentModel>>(content) : new List<StudentModel>();
+
+                if (list != null && list.Any())
+                {
+                    list = list.Where(x => x.Id != id).ToList();
+                }
+
+                if (list != null)
+                {
+                    var newContent = JsonConvert.SerializeObject(list);
+                    if (!string.IsNullOrWhiteSpace(newContent))
+                    {
+                        System.IO.File.WriteAllText(filePath, newContent);
+
+                        return RedirectToAction("StudentList2");
+                    }
+                }
+            }
+
+            return RedirectToAction("StudentList2");
+        }
+
+        #endregion
     }
 }
--- FirstApp/Models/StudentModel.cs
+++ FirstApp/Models/StudentModel.cs
@@ -18,5 +18,6 @@
         public string Class { get; set; }
         public string Section { get; set; }
         public string Email { get; set; }
+        public string PhoneNumber { get; set; }
     }
 }
